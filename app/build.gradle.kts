e4c88eb825a51f4e6d70542395c6d00176ff7c2a
miniworldcnmm <miniworldcnmm@gmail.com>
2026-04-14 20:49:31 +0800

    修复core依赖引用错误：把core.ktx改成普通core

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.core)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
 
